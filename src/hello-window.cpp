965941a267fb35902e84f58140733384f21787a5
Abraham <bioRex21@gmail.com>
2021-09-19 14:53:32 -0700

    Use uniform to change color

--- src/hello-window.cpp
+++ src/hello-window.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <cmath>
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
@@ -18,10 +19,11 @@ const unsigned int SCR_HEIGHT = 600;
                                  "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
+                                    "uniform vec4 ourColor;\n"
                                    "out vec4 FragColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   "   FragColor = vec4(0.0f, 0.0f, 0.9f, 1.0f);\n"
+                                   "   FragColor = ourColor;\n"
                                    "}\n\0";
 
 
@@ -144,7 +146,14 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         // OUR DRAWING
-        glUseProgram(shaderProgram);
+        // be sure to activate the shader
+    glUseProgram(shaderProgram);// use program firts, before updating the uniform
+  
+    // update the uniform color
+    float timeValue = glfwGetTime();
+    float greenValue = sin(timeValue) / 2.0f + 0.5f;
+    int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0); // no need to unbind it every time

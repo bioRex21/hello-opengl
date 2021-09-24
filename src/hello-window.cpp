356d13eb392764f931e7323f86326353ded4fbab
Abraham <bioRex21@gmail.com>
2021-09-24 15:55:18 -0700

    Add VAO comments

--- src/hello-window.cpp
+++ src/hello-window.cpp
@@ -108,23 +108,22 @@ int main()
         0.0f, 0.5f, 0.0f};
 
     unsigned int VBO;
-    unsigned int VAO;
+    unsigned int VAO; //it will save the GL states config
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
 
     //* bind the Vertex Array Object first, then bind and set vertex buffer(s),
     //* and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
+    glBindVertexArray(VAO); //activating to save the following states:
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO
     //as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // turn off saving state, we're done. Now can use it at render loop.
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO,
     //but this rarely happens. Modifying other
@@ -152,14 +151,16 @@ int main()
     // update the uniform color
     float timeValue = glfwGetTime();
     float greenValue = sin(timeValue) / 2.0f + 0.5f;
+    // input our data
     int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
     glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+       // using the sate we saved previously
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0); // no need to unbind it every time
 
-        //OUR DRAWING ENDS
 
+        //OUR DRAWING ENDS
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);

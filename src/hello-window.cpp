10e111adf970fedcc82bf22470ac2cf795811883
Abraham <bioRex21@gmail.com>
2021-10-10 11:21:57 -0700

    Use EBO to draw rectangle

--- src/hello-window.cpp
+++ src/hello-window.cpp
@@ -47,15 +47,24 @@ int main()
         return -1;
     }
 
+// 4 vertices to create a rectangle (made by 2 triangles).
     float vertices[] = {
-        // positions         // colors
-        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
-        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // top
-    };
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+};
+
+// indicate which vertices to use for first and second triangle
+unsigned int indices[] = {  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+}; 
 
     unsigned int VBO;
     unsigned int VAO; //it will save the GL states config
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
 
@@ -67,11 +76,17 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+//when using color in same vertices (xyz + rgb = 6) glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
-    // color attribute (rgb)
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+    // color attribute (rgb)// not being used in this example
+    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    //glEnableVertexAttribArray(1);
+
+    
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO
     //as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
@@ -103,7 +118,8 @@ int main()
        ourShader.use();
         // using the sate we saved previously
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         // glBindVertexArray(0); // no need to unbind it every time
 
         //OUR DRAWING ENDS
